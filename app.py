7573345f0b1e0483d40aabf43b9bb8416f2b12c5
Shoaibvanu <156096634+Shoaibvanu@users.noreply.github.com>
2024-03-15 13:59:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
+import openai
 import io
 
 
