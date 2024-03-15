6bd70b605eb7210f0cd4442c9a384972cd971ffd
Shoaibvanu <156096634+Shoaibvanu@users.noreply.github.com>
2024-03-15 13:55:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 import streamlit as st
-from dotenv import load_dotenv
 from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import HuggingFaceInstructEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chat_models import ChatOpenAI
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain
+from langchain-community.text_splitter import CharacterTextSplitter
+from langchain-community.embeddings import HuggingFaceInstructEmbeddings
+from langchain-community.vectorstores import FAISS
+from langchain-community.chat_models import ChatOpenAI
+from langchain-community.memory import ConversationBufferMemory
+from langchain-community.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
-from langchain.llms import HuggingFaceHub
+from langchain-community.llms import HuggingFaceHub
 import fitz  
 import io
 
